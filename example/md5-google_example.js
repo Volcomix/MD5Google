e2c147855bc8085cfb6167931f5dff5e6d8b05c0
Volcomix <Volcomix@users.noreply.github.com>
2014-10-24 15:00:39 +0200

    Using promises with Q

--- example/md5-google_example.js
+++ example/md5-google_example.js
@@ -5,4 +5,14 @@ var md5Google = require('../lib/md5-google.js');
 md5Google.awesome();
 // => awesome
 
-md5Google.decrypt("f20f8505ac09ae2fe6fe2e68976e80e4");
+console.log();
+md5Google.decrypt("f20f8505ac09ae2fe6fe2e68976e80e4")
+.then(function(result) {
+    console.log(result);
+})
+.catch(function(reason) {
+    console.error(reason);
+})
+.done(function() {
+    console.log();
+});
