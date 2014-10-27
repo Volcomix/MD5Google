2d546454065009a99fd4ff6b0978c1a6c11ddeec
Volcomix <Volcomix@users.noreply.github.com>
2014-10-27 17:05:45 +0100

    Adding Mocha tests, fixing some JSlint hints.

--- example/md5-google_example.js
+++ example/md5-google_example.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 'use strict';
 
 var md5Google = require('../lib/md5-google.js');
@@ -6,13 +7,13 @@ md5Google.awesome();
 // => awesome
 
 console.log();
-md5Google.decrypt("f20f8505ac09ae2fe6fe2e68976e80e4")
-.then(function(result) {
+md5Google.decrypt('f20f8505ac09ae2fe6fe2e68976e80e4')
+.then(function (result) {
     console.log(result);
 })
-.catch(function(reason) {
+.catch(function (reason) {
     console.error(reason);
 })
-.done(function() {
+.done(function () {
     console.log();
 });
--- test/md5-google_test.js
+++ test/md5-google_test.js
@@ -1,10 +1,17 @@
-/*global describe,it*/
+/*jslint node: true */
+/*global describe, it */
 'use strict';
 var assert = require('assert'),
-  md5Google = require('../lib/md5-google.js');
+    md5Google = require('../lib/md5-google.js');
 
-describe('md5-google node module.', function() {
-  it('must be awesome', function() {
-    assert( md5Google.awesome(), 'awesome');
-  });
+describe('md5-google node module.', function () {
+    it('must be awesome', function () {
+        assert(md5Google.awesome(), 'awesome');
+    });
+    
+    describe('#decrypt', function () {
+        it('should decrypt "logica" for each parser excepting "www.filehash.info"', function () {
+            assert(md5Google.decrypt('f20f8505ac09ae2fe6fe2e68976e80e4'));
+        });
+    });
 });
