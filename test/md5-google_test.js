6a4c14b78b616f86241de57ff852e08206204e3f
Volcomix <Volcomix@users.noreply.github.com>
2014-10-30 15:22:50 +0100

    Add test when no result found

--- test/md5-google_test.js
+++ test/md5-google_test.js
@@ -7,7 +7,7 @@ var assert = require('assert'),
 describe('md5-google node module.', function () {
     describe('#decrypt.all()', function () {
         function testDecryptAll(md5, expected, fulfilledHostnames, rejectedHostnames, only) {
-            (only ? describe.only : describe)(md5, function () {
+            (only ? describe.only : describe)('(' + md5 + ': ' + expected + ')', function () {
                 var md5Results,
                     hostnamesCount = fulfilledHostnames.length + rejectedHostnames.length;
 
@@ -21,6 +21,7 @@ describe('md5-google node module.', function () {
                 fulfilledHostnames.forEach(function (hostname) {
                     it('should return "' + expected + '" with "' + hostname + '" website', function () {
                         var result = md5Results[hostname];
+                        assert(result, JSON.stringify(md5Results, null, 4));
                         assert.equal(result.state, 'fulfilled', JSON.stringify(result, null, 4));
                         assert.equal(result.value, expected, JSON.stringify(result, null, 4));
                     });
@@ -29,6 +30,7 @@ describe('md5-google node module.', function () {
                 rejectedHostnames.forEach(function (hostname) {
                     it('should not find MD5 parser for "' + hostname + '" website', function () {
                         var result = md5Results[hostname];
+                        assert(result, JSON.stringify(md5Results, null, 4));
                         assert.equal(result.state, 'rejected', JSON.stringify(result, null, 4));
                         assert.equal(result.reason, 'No MD5 parser found', JSON.stringify(result, null, 4));
                     });
@@ -48,14 +50,14 @@ describe('md5-google node module.', function () {
             'f20f8505ac09ae2fe6fe2e68976e80e4',
             'logica',
             ['www.md5-hash.com', 'md5.db30.com', 'md5-online.ru', 'de.md5decoder.org', 'www.md5center.com', 'md5info.ru', 'md5this.com'],
-            ['www.filehash.info']
+            []
         );
 
         testDecryptAll(
             '9d0c2b5e15cc600a9828a18a5ffe7dba',
             '123soleil',
             ['md5.znaet.org', 'www.md5this.com'],
-            ['forum.antichat.net', 'cegica.googlecode.com', 'www.stafaband.info', 'insidepro.com', 'pastebin.ca', 'paste2.org', 'dopeco.de', 'pastebin.com']
+            ['forum.antichat.net', 'cegica.googlecode.com', 'www.stafaband.info', 'forum.insidepro.com', 'insidepro.com', 'pastebin.ca', 'paste2.org', 'dopeco.de']
         );
     });
     
@@ -71,6 +73,16 @@ describe('md5-google node module.', function () {
             return md5Google.decrypt('9d0c2b5e15cc600a9828a18a5ffe7dba')
             .then(function (result) {
                 assert.equal(result, '123soleil');
+            });
+        });
+        
+        it('should decrypt MD5 "a6c4e4ce4ab075c33b80986c1b00125a" for "Un8r34k48l3"', function () {
+            return md5Google.decrypt('a6c4e4ce4ab075c33b80986c1b00125a')
+            .then(function (result) {
+                assert.fail(result, 'No result found', 'A result was found', '==');
+            })
+            .catch(function (reason) {
+                assert.equal(reason, 'No result found');
             });
         });
     });
